4b10703f4812f1a8008dfda19e6f457b85d63d8e
saeki1120cat <83437191+saeki1120cat@users.noreply.github.com>
2021-07-06 17:00:48 +0800

    Add files via upload

---
+++ crawler_jobSearch.py
@@ -0,0 +1,224 @@
+import requests
+from bs4 import BeautifulSoup
+from fake_useragent import UserAgent
+import time
+import random
+import concurrent.futures
+import pandas as pd
+
+def seek_104(url):
+    global df
+    date = []
+    title = []
+    company = []
+    salary = []
+    link = []
+    job_desc = []
+    location = []
+
+    sleep_time = random.random()
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    jobs = soup.find_all('div', class_='b-block__left')
+
+    for job in jobs:
+        try:
+            date.append(job.find('span', class_='b-tit__date').text.replace('\n', ''))
+        except:
+            date.append('-')
+
+        try:
+            title.append(job.find('a', class_='js-job-link').text)
+        except:
+            title.append('-')
+
+        try:
+            company.append(job.find('ul', class_='b-list-inline b-clearfix').text.replace('\n', ''))
+        except:
+            company.append('-')
+
+        try:
+            salary.append(job.find('div', class_='job-list-tag b-content').text)
+        except:
+            salary.append('-')
+
+        try:
+            link.append('https:' + job.find('a').get('href'))
+        except:
+            link.append('-')
+
+        try:
+            job_desc.append(job.find('p', class_='job-list-item__info b-clearfix b-content').text.replace('\n', ' '))
+        except:
+            job_desc.append('-')
+
+        try:
+            location.append(job.find('ul', class_='b-list-inline b-clearfix job-list-intro b-content').find("li").text)
+        except:
+            location.append('-')
+
+    data_dict = {"刊登日期": date, "職稱": title, "公司名稱": company, "公司地點": location, "描述": job_desc, "薪資": salary,
+                 "webURL": link}
+    df = df.append(pd.DataFrame(data_dict))
+    indexNames = df[ df['職稱'] == '-' ].index
+    df.drop(indexNames , inplace=True)
+    time.sleep(sleep_time)
+    return df
+
+
+def seek_1111(url):
+    global df
+    date = []
+    title = []
+    company = []
+    location = []
+    salary = []
+    link = []
+    job_desc = []
+
+    sleep_time = random.random()
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    jobs = soup.find_all('div', class_='item__job')
+    for job in jobs:
+        try:
+            title.append(job.find('div', class_='item__job-position0 item__m--link').text)
+        except:
+            title.append('-')
+
+        try:
+            link.append('https:' + job.find('a').get('href'))
+        except:
+            link.append('-')
+
+        try:
+            company.append(job.find('div', class_='item__job-organ-m').get('aria-label'))
+        except:
+            company.append('-')
+
+        try:
+            location.append(job.find('i', class_='item__job-prop-item item__job-prop-workcity').get('aria-label'))
+        except:
+            location.append('-')
+
+        try:
+            salary.append(job.find('i', class_='item__job-prop-item item__job-prop-salary').get('aria-label'))
+        except:
+            salary.append('-')
+
+        try:
+            date.append(job.find('div', class_='item__job-control-item item__job-control-datechange').get('data-mmdd'))
+        except:
+            date.append('-')
+
+        try:
+            job_desc.append(
+                job.find('div', class_='item__job-desc item__job-desc-un_extension').get('title').replace('\n', ' '))
+        except:
+            job_desc.append('-')
+
+    data_dict = {"刊登日期": date, "職稱": title, "公司名稱": company, "公司地點": location, "描述": job_desc, "薪資": salary,
+                 "webURL": link}
+    df = df.append(pd.DataFrame(data_dict))
+    time.sleep(sleep_time)
+    return df
+
+
+def seek_cakeresume(url):
+    global df
+    title = []
+    link = []
+    company = []
+    job_desc = []
+    date = []
+    location = []
+    salary = []
+
+    sleep_time = random.random()
+    df = pd.DataFrame()
+
+    ua = UserAgent()
+    headers = {'User-Agent': ua.random}
+
+    res = requests.get(url, headers=headers)
+    soup = BeautifulSoup(res.text, 'html.parser')
+    jobs = soup.find_all('div', class_='job-item')
+    jobs2 = soup.find_all('div', class_='meta-container')
+
+    for job in jobs:
+        try:
+            title.append(job.find('div', class_='job-link-wrapper').text)
+        except:
+            title.append('-')
+
+        try:
+            link.append(job.find('a', class_='job-link').get('href'))
+        except:
+            link.append('-')
+
+        try:
+            company.append(job.find('h5', class_='page-name').text)
+        except:
+            company.append('-')
+
+        try:
+            job_desc.append(job.find('p', class_='job-desc').text.replace('\n', ''))
+        except:
+            job_desc.append('-')
+
+        try:
+            location.append(job.find('div', class_='location-section').text)
+        except:
+            location.append('-')
+
+        try:
+            salary.append(job.find('span', class_='job-salary').text)
+        except:
+            salary.append('-')
+
+    for job2 in jobs2:
+        try:
+            date.append(job2.find('span', class_='update-section').text)
+        except:
+            date.append('-')
+
+    data_dict = {"刊登日期": date, "職稱": title, "公司名稱": company, "公司地點": location, "描述": job_desc, "薪資": salary,
+                 "webURL": link}
+    df = df.append(pd.DataFrame(data_dict))
+    time.sleep(sleep_time)
+    return df
+
+def main(web, keyword, end_page):
+    global df, urls
+    df = pd.DataFrame()
+    if web =='104':
+        urls = [f'https://www.104.com.tw/jobs/search/?keyword={keyword}&order=11&asc=0&page={str(i)}&mode=s' for i in range(1,end_page+1)]
+    elif web =='cakeresume':
+        urls = [f'https://www.cakeresume.com/jobs?q={keyword}&refinementList%5Border%5D=latest&page={str(i)}' for i in range(1,end_page+1)]
+    elif web =='1111':
+        urls = [f'https://www.1111.com.tw/search/job?ks={keyword}&fs=1&page={str(i)}&col=da&sort=desc' for i in range(1,end_page+1)]
+
+
+# 執行爬蟲並進行多工處理
+def crawler_jobSearch(web):
+    if web == '104':
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            executor.map(seek_104, urls)
+
+    elif web == 'cakeresume':
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            executor.map(seek_cakeresume, urls)
+
+    elif web == '1111':
+        with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
+            executor.map(seek_1111, urls)
+
+
+
---
+++ main.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, render_template
+import crawler_jobSearch as cj
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def poker():
+    if request.method == 'GET':
+        outStr = """
+        <html>
+            <head>
+                <title>job search</title>
+            </head>
+            <body>
+                <h1><font face="fantasy" color="#4F4FFF">Let's start to search jobs!</font></h1>
+                <form action="/" method="post">
+                      <h2><font face="monospace" color="#00D1D1">Which website do you want to use?</font></h2>
+                      <select name="web" style="font-size:25px;">
+                          <option value="cakeresume">cakeresume</option>
+                          <option value="104">104</option>
+                          <option value="1111">1111</option>
+                      </select>
+                      <br>
+                      <h2><font face="monospace" color="#00D1D1">Please enter the keyword:</font></h2>
+                          <input type="textbox" name="keyword">
+                      <br>
+                      <h2><font face="monospace" color="#00D1D1">Please enter the end page:</font></h2>
+                          <input type="textbox" name="end_page">
+                      <br>
+                      <br>
+                      <button type="submit" style="width:120px;height:40px;font-size:20px;">Submit</button>
+                </form>
+            </body>
+        </html>
+        """
+        return outStr
+    elif request.method == 'POST':
+        web = str(request.form.get('web'))
+        keyword = str(request.form.get('keyword'))
+        end_page = int(request.form.get('end_page'))
+        cj.main(web, keyword, end_page)
+        cj.crawler_jobSearch(web)
+        df = cj.df
+        return render_template('result.html', column_names=df.columns.values, row_data=list(df.values.tolist()), zip=zip)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
